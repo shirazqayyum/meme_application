1022d128b477120142fbca0740344856403f7632
shiraz <q.shiraz@gmail.com>
2014-03-20 20:28:46 -0400

    plotting all 3 graphs

--- plotting.m
+++ plotting.m
@@ -41,7 +41,89 @@ errorbar(x, mean(avg_simple_range_50, 1)*b, std(avg_simple_range_50, 0,1)/8,'r--
     'MarkerFaceColor',[.94,0.99,0.0]);
 xlabel('no. of memory values used','FontSize', 15) % x-axis label
 ylabel('% of successful downloads','FontSize', 15) % y-
+
+legend('MEME','Without direction knowledge')
+annotation('textbox', [0.58,0.6,0.1,0.1],...
+           'String', 'Communication range = 50m');
 hold off
-                                                                        
+            
+
+
+
+
+
+
+% ############### plot range 100 ################## %
+
+avg_mem_range_100 = zeros(1000, mem_max);
+x = 1 : mem_max;
+for i = 1 : mem_max
+    val = sum(results_mem_range_100(:, :, i), 2);
+    val = val ./ size(results_mem_range_100,2);
+    avg_mem_range_100(:, i) = val;
+end
+avg_mem_range_100 = avg_mem_range_100 * 100;
+
+
+avg_simple_range_100 = zeros(1000, mem_max);
+x = 1 : mem_max;
+for i = 1 : mem_max
+    val = sum(results_simple_range_100(:, :, i), 2);
+    val = val ./ size(results_simple_range_100,2);
+    avg_simple_range_100(:, i) = val;
+end
+avg_simple_range_100 = avg_simple_range_100 * 100;
+
+figure
+hold on
+errorbar(x, mean(avg_mem_range_100, 1)*b, std(avg_mem_range_100, 0,1)/8,'-.bs', 'MarkerSize' , 10, 'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[0.1,0.56,0.61]);
+errorbar(x, mean(avg_simple_range_100, 1)*b, std(avg_simple_range_100, 0,1)/8,'r--o', 'MarkerSize' , 10, 'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[.94,0.99,0.0]);
+xlabel('no. of memory values used','FontSize', 15) % x-axis label
+ylabel('% of successful downloads','FontSize', 15) % y-
+
+legend('MEME','Without direction knowledge')
+annotation('textbox', [0.58,0.6,0.1,0.1],...
+           'String', 'Communication range = 100m');
+hold off
+
+
+
+
+% ############### plot range 50 ################## %
+
+avg_mem_range_150 = zeros(1000, mem_max);
+x = 1 : mem_max;
+for i = 1 : mem_max
+    val = sum(results_mem_range_150(:, :, i), 2);
+    val = val ./ size(results_mem_range_150,2);
+    avg_mem_range_150(:, i) = val;
+end
+avg_mem_range_150 = avg_mem_range_150 * 100;
+
+
+avg_simple_range_150 = zeros(1000, mem_max);
+x = 1 : mem_max;
+for i = 1 : mem_max
+    val = sum(results_simple_range_150(:, :, i), 2);
+    val = val ./ size(results_simple_range_150,2);
+    avg_simple_range_150(:, i) = val;
+end
+avg_simple_range_150 = avg_simple_range_150 * 100;
+
+figure
+hold on
+errorbar(x, mean(avg_mem_range_150, 1)*b, std(avg_mem_range_150, 0,1)/8,'-.bs', 'MarkerSize' , 10, 'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[0.1,0.56,0.61]);
+errorbar(x, mean(avg_simple_range_150, 1)*b, std(avg_simple_range_150, 0,1)/8,'r--o', 'MarkerSize' , 10, 'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[.94,0.99,0.0]);
+xlabel('no. of memory values used','FontSize', 15) % x-axis label
+ylabel('% of successful downloads','FontSize', 15) % y-
+
+legend('MEME','Without direction knowledge')
+annotation('textbox', [0.58,0.6,0.1,0.1],...
+           'String', 'Communication range = 150m');
+hold off
 
 %ciplot(mean(avg_mem_range_50, 1), -1*mean(avg_mem_range_50, 1))
